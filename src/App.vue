62d55bf05fd9675116ec859e0fd4da6147b52dcf
Michael Frohlich <mike.frohl@gmail.com>
2019-09-07 21:35:45 +0200

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CharacerSheet msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/CharacerSheet.vue'
+import CharacerSheet from './components/CharacerSheet.vue'
 
 export default {
   name: 'app',
